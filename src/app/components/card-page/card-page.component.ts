ef28b3376e89b4ded55fdd549e70e7c522e38177
Kyle J. Kemp <kyle@seiyria.com>
2026-07-31 06:26:59 -0500

    ang21

--- src/app/components/card-page/card-page.component.ts
+++ src/app/components/card-page/card-page.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 type CardSize = 'main' | 'sub' | 'heroes' | 'heroes-sub';
 
 @Component({
   selector: 'app-card-page',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './card-page.component.html',
   styleUrl: './card-page.component.scss',
 })
