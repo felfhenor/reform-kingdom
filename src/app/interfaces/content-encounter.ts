0ac4fd804ac28c6ae0dc0275fa18603fe6cbc5b5
Kyle J. Kemp <kyle@seiyria.com>
2026-08-02 14:15:03 -0500

    add remaining files

---
+++ src/app/interfaces/content-encounter.ts
@@ -0,0 +1,28 @@
+import type { MonsterId } from '@interfaces/content-monster';
+import type { Branded, IsContentItem } from '@interfaces/identifiable';
+import type { HasDescription } from '@interfaces/traits';
+
+export type EncounterId = Branded<string, 'EncounterId'>;
+
+export type EncounterLevelRange = {
+  min: number;
+  max: number;
+};
+
+export type EncounterFightMonster = {
+  monsterId: MonsterId;
+};
+
+export type EncounterFight = {
+  monsters: EncounterFightMonster[];
+};
+
+export type EncounterContent = IsContentItem &
+  HasDescription & {
+    id: EncounterId;
+    __type: 'encounter';
+
+    levelRange: EncounterLevelRange;
+
+    fights: EncounterFight[];
+  };
