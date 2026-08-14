f320bbfde3f16030ae439d46357716bdc8550e09
Kyle J. Kemp <kyle@seiyria.com>
2026-08-14 10:04:46 -0500

    add gameanalytics keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   platform: 'web-dev',
   gameanalytics: {
-    game: '',
-    secret: '',
+    game: '19e144753faa9242f86e5a4c29b4c7b2',
+    secret: '670bf61dfb6fe55dd1c21b3232e5d668bf332f7c',
   },
   rollbar: {
     accessToken: 'c2e6762edfaa47d1ac28de92f66b0529',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,12 @@ export const environment = {
   production: true,
   platform: 'web',
   gameanalytics: {
-    game: '',
-    secret: '',
+    game: '27cb0b261b9161eebf9022f5bf78b762',
+    secret: '588d18b838a973db3ca996579b47f5d12266bf40',
   },
   rollbar: {
-    accessToken: 'c43dd56f173f48c4837648a054f32749b9a40619068e3ffa646b9abaf329f21e593c91c2f7f9a3db33550457717b9b08',
+    accessToken:
+      'c43dd56f173f48c4837648a054f32749b9a40619068e3ffa646b9abaf329f21e593c91c2f7f9a3db33550457717b9b08',
     hostBlockList: ['netlify.app'],
     captureUncaught: true,
     captureUnhandledRejections: true,
