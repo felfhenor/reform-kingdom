45466552605c5a5d40cb2d227bb4161c5851fffe
Kyle J. Kemp <kyle@seiyria.com>
2026-08-14 14:44:58 -0500

    add more hotkeys!

--- src/app/components/button-kingdom-back/button-kingdom-back.component.ts
+++ src/app/components/button-kingdom-back/button-kingdom-back.component.ts
@@ -1,14 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { kingdomSubviewClear } from '@helpers/ui';
+import { TippyDirective } from '@ngneat/helipopper';
+import { HotkeysDirective } from '@ngneat/hotkeys';
 
 @Component({
   selector: 'app-button-kingdom-back',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [TippyDirective, HotkeysDirective],
   template: `
     <button
       class="btn btn-sm btn-neutral btn-outline text-neutral-content"
+      tp="Go Back [BACKSPACE]"
       (click)="back()"
+      [hotkeys]="'BACKSPACE'"
+      (hotkey)="back()"
+      isGlobal
     >
       Back
     </button>
