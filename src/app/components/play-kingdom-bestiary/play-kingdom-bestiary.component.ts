76a6acc75aac9790d41660b4bf38cc44f26a77a7
Kyle J. Kemp <kyle@seiyria.com>
2026-09-09 08:34:50 -0500

    feat(ux): make the bestiary/worker page resemble infusion page

--- src/app/components/play-kingdom-bestiary/play-kingdom-bestiary.component.ts
+++ src/app/components/play-kingdom-bestiary/play-kingdom-bestiary.component.ts
@@ -9,6 +9,7 @@ import { ButtonKingdomBackComponent } from '@components/button-kingdom-back/butt
 import { CardPageComponent } from '@components/card-page/card-page.component';
 import { DetailBestiaryMonsterComponent } from '@components/detail-bestiary-monster/detail-bestiary-monster.component';
 import { IconUnknownComponent } from '@components/icon-unknown/icon-unknown.component';
+import { SlotIconBlankComponent } from '@components/slot-icon-blank/slot-icon-blank.component';
 import { SFXDirective } from '@directives/sfx.directive';
 import {
   filterBestiaryEntries,
@@ -26,6 +27,7 @@ import type { MonsterId } from '@interfaces';
     IconUnknownComponent,
     ButtonKingdomBackComponent,
     SFXDirective,
+    SlotIconBlankComponent,
   ],
   templateUrl: './play-kingdom-bestiary.component.html',
   styleUrl: './play-kingdom-bestiary.component.scss',
